f0de110425a41b5a23cfad9ae72e3a55b84fdcf7
Suraj1999 <102948555+Suraj1999@users.noreply.github.com>
2022-07-15 00:48:07 +0530

    Add files via upload

---
+++ first/first.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { first } from "rxjs";
+
+@Component({
+    selector:'app-first',
+    templateUrl:'./first.component.html',
+    styleUrls: ['./first.component.css']
+})
+export class FirstComponent{
+
+}
